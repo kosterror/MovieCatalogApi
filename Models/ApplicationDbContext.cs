2b2bbd0c86dd56db97b1cef94e9815138f2c25be
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 18:46:37 +0700

    Added entities in ApplicationDbContext

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MovieCatalogApi.Models.Entities;
 
 namespace MovieCatalogApi.Models;
 
 public class ApplicationDbContext : DbContext
 {
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<MovieEntity> Movies { get; set; }
+    public DbSet<ReviewEntity> Reviews { get; set; }
+    public DbSet<GenreEntity> Genres { get; set; }
     public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
     {
         Database.EnsureCreated();
