f23bf905c980aeb90d78e6160126f07bdd510902
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 16:42:01 +0700

    Created MovieEntity, ReviewEntity, UserEntity

---
+++ Models/Entities/MovieEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace MovieCatalogApi.Models.Entities;
+
+public class MovieEntity
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string Description { get; set; }
+    public int Year { get; set; }
+    public string Country { get; set; }
+    public int Time { get; set; }
+    public string Tagline { get; set; }
+    public string Director { get; set; }
+    public int Budget { get; set; }
+    public int Fees { get; set; }
+    public int AgeLimit { get; set; }
+    public GenreEntity Genre { get; set; }
+}
---
+++ Models/Entities/ReviewEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieCatalogApi.Models.Entities;
+
+public class ReviewEntity
+{
+    public Guid Id { get; set; }
+    public UserEntity User { get; set; }
+    public MovieEntity Movie { get; set; }
+    public string ReviewText { get; set; }
+    public int Rating { get; set; }
+    public bool IsAnonymous { get; set; }
+    public DateTime CreatedDateTime { get; set; }
+}
