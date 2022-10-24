70128f003d9e130fba215267ad3a58c1572dbffd
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 20:10:35 +0700

    Created UserRegisterDto

---
+++ Models/Dtos/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieCatalogApi.Models.Dtos;
+
+public class UserRegisterDto
+{
+    public string userName;
+    public string name;
+    public string password;
+    public string email;
+    public DateTime birthDate;
+    public Gender gender;
+}
