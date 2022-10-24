e6083a74bc3eee2712d30024d9f5a396b2b342f1
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 22:16:14 +0700

    Created ReviewDto

---
+++ Models/Dtos/ReviewDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieCatalogApi.Models.Dtos;
+
+public class ReviewDto
+{
+    public Guid id { get; set; }
+    public int rating { get; set; }
+    public string reviewText { get; set; }
+    public bool isAnonymous { get; set; }
+    public DateTime createDateTime { get; set; }
+    public UserShortDto author { get; set; }
+}
