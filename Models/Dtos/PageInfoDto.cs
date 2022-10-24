7c081ca8d27f63e345b53f20e5b0702c35c35f14
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 22:07:44 +0700

    Created PageInfoDto

---
+++ Models/Dtos/PageInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieCatalogApi.Models.Dtos;
+
+public class PageInfoDto
+{
+    public int pageSize { get; set; }
+    public int pageCount { get; set; }
+    public int currentPage { get; set; }
+}
