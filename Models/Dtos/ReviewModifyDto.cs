34e969add2151b935d9caf03b51eb9f872d5f4f7
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 22:30:14 +0700

    Created ReviewModifyDto

---
+++ Models/Dtos/ReviewModifyDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCatalogApi.Models.Dtos;
+
+public class ReviewModifyDto
+{
+    public string reviewText { get; set; }
+    [Range(0, 10)]
+    public int rating { get; set; }
+    public bool isAnonymous { get; set; }
+}
