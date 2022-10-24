04c03b74803c3011529450bce1ecfa241282926f
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 22:20:09 +0700

    Created ProfileDto

---
+++ Models/Dtos/ProfileDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieCatalogApi.Models.Dtos;
+
+public class ProfileDto
+{
+    public Guid id { get; set; }
+    public string nickName { get; set; }
+    public string email { get; set; }
+    public string avatarLink { get; set; }
+    public string name { get; set; }
+    public DateTime birthDate { get; set; }
+    public Gender gender { get; set; }
+}
