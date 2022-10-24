1bee051c658fad516fdce5ddf4468d845561799f
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 22:12:51 +0700

    Created UserShortDto

---
+++ Models/Dtos/UserShortDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieCatalogApi.Models.Dtos;
+
+public class UserShortDto
+{
+    public Guid userId { get; set; }
+    public string nickName { get; set; }
+    public string avatar { get; set; }
+}
