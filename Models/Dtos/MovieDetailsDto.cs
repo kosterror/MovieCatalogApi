f73850c64e5555819b982bd985667b0e8f000aa5
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 22:25:33 +0700

    Created MovieDetailsDto

---
+++ Models/Dtos/MovieDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿namespace MovieCatalogApi.Models.Dtos;
+
+public class MovieDetailsDto
+{
+    public Guid id { get; set; }
+    public string name { get; set; }
+    public string poster { get; set; }
+    public int year { get; set; }
+    public string country { get; set; }
+    public List<GenreDto> genres { get; set; } = new();
+    public List<ReviewDto> reviews { get; set; } = new();
+    public int time { get; set; }
+    public string tagline { get; set; }
+    public string description { get; set; }
+    public string director { get; set; }
+    public int budget { get; set; }
+    public int fees { get; set; }
+    public int ageLimit { get; set; }
+}
