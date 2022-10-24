df3596b3724198fbdfe54fbceccc51c573ecd3b2
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-24 22:10:53 +0700

    Created MoviesPagedListDto

---
+++ Models/Dtos/MoviesPagedListDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieCatalogApi.Models.Dtos;
+
+public class MoviesPagedListDto
+{
+    public List<MovieElementDto> movies { get; set; }
+    public PageInfoDto pageInfo { get; set; }
+}
