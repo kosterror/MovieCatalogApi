5babbdf1c13d4acdde6467205989150ae8cd31b5
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-30 14:19:44 +0700

    Created sql scripts

---
+++ Models/PsqlScripts/genres_of_movie.sql
@@ -0,0 +1,7 @@
+﻿-- для фореста гампа
+insert into "GenreEntityMovieEntity" ("GenresId", "MoviesId")
+values ('3b781828-f329-4785-aecd-08d9b9f3d27c', 'b6c5228b-91fb-43a1-a2ac-08d9b9f3d2a2'),
+       ('b4e44ca7-7b96-4d12-aed3-08d9b9f3d27c', 'b6c5228b-91fb-43a1-a2ac-08d9b9f3d2a2'),
+       ('5fb99899-1833-4e9e-aed7-08d9b9f3d27c', 'b6c5228b-91fb-43a1-a2ac-08d9b9f3d2a2'),
+       ('279e470a-11dc-46e2-aeda-08d9b9f3d27c', 'b6c5228b-91fb-43a1-a2ac-08d9b9f3d2a2'),
+       ('9bba1883-a1fc-448a-aedb-08d9b9f3d27c', 'b6c5228b-91fb-43a1-a2ac-08d9b9f3d2a2');
