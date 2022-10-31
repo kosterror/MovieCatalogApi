9aec59523610b190779a3064460c9ebcbf5aba8e
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-10-31 21:59:19 +0700

    Realized adding movie to favourites

--- Services/Implementations/FavoriteMoviesService.cs
+++ Services/Implementations/FavoriteMoviesService.cs
@@ -1,4 +1,6 @@
-﻿using MovieCatalogApi.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCatalogApi.Exceptions;
+using MovieCatalogApi.Models;
 using MovieCatalogApi.Models.Dtos;
 
 namespace MovieCatalogApi.Services.Implementations;
@@ -19,7 +21,30 @@ public class FavoriteMoviesService : IFavoriteMoviesService
 
     public void AddFavourite(string userName, Guid movieId)
     {
-        throw new NotImplementedException();
+        var userEntity = _context.Users
+            .Include(x => x.FavoriteMovies)
+            .FirstOrDefault(x => x.UserName == userName);
+        
+        var movieEntity = _context.Movies.FirstOrDefault(x => x.Id == movieId);
+        
+        if (userEntity == null)
+        {
+            throw new PermissionDeniedException("User with this token was not found");
+        }
+
+        if (movieEntity == null)
+        {
+            throw new NotFoundException("Film with this ID was not found");
+        }
+
+        if (userEntity.FavoriteMovies.Exists(x => x.Id == movieId))
+        {
+            throw new BadRequestException("Movie have already added to favorites");
+        }
+        
+        userEntity.FavoriteMovies.Add(movieEntity);
+
+        _context.SaveChanges();
     }
 
     public void DeleteFavourite(string userName, Guid movieId)
