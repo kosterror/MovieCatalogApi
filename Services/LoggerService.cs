312c7a5c71bf31367f0478122b7edd2b3d7e46d6
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2022-11-09 11:08:29 +0700

    Added creating of directory if it necessary for LoggerService

--- Services/LoggerService.cs
+++ Services/LoggerService.cs
@@ -4,6 +4,9 @@ public class LoggerService : ILoggerService
 {
     private const string FileDirectory = "./Logs/";
     
+    /*
+     * Оболочка для Log, к сообщению добавится время и статус
+     */
     public async Task LogInfo(string message)
     {
         var path = GetPathFile();
@@ -11,6 +14,9 @@ public class LoggerService : ILoggerService
         await Log(path, $"{DateTime.UtcNow.ToLongTimeString()} |INF| {message}");          
     }
     
+    /*
+     * Оболочка для Log, к сообщению добавится время и статус
+     */
     public async Task LogError(string message)
     {
         var path = GetPathFile();
@@ -19,6 +25,9 @@ public class LoggerService : ILoggerService
     }
 
     
+    /*
+     * Просто запись в файл с заданным путём и сообщением
+     */
     private static async Task Log(string pathFile, string message)
     {
         await using var writer = new StreamWriter(pathFile, true);
@@ -26,8 +35,17 @@ public class LoggerService : ILoggerService
         writer.Close();
     }
 
+    /*
+     * Если необходимо, то создаст директорию для логов
+     * Если файл с таким названием еще не существует, то создаст такой файл
+     */
     private static void CreateLogFile(string pathFile)
     {
+        if (!Directory.Exists(FileDirectory))
+        {
+            var directoryInfo = Directory.CreateDirectory(FileDirectory);
+        }
+        
         var fileInfo = new FileInfo(pathFile);
         
         if (fileInfo.Exists) return;
@@ -36,6 +54,10 @@ public class LoggerService : ILoggerService
         fileStream.Close();
     }
 
+    /*
+     * Для каждой даты свой файлик с логами для более удобной навигации по ним
+     * Название файла имеет следующий шаблон: "{дд.мм.гггг}.txt"
+     */
     private static string GetPathFile()
     {
         return $"{FileDirectory}{DateTime.UtcNow.ToShortDateString()}.txt";
